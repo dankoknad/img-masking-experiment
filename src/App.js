efcb17547b42f6e64bc9e54701a909d277a1d51f
Danko-M <dankomilutinovic@gmail.com>
2016-12-22 15:33:29 +0100

    improvements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function MaskedImage({path, width, height}) { 
+function MaskedImage({path, width, height, link}) { 
    return (
-      <svg width={width} height={height} x="0px" y="0px" viewBox="0 0 1920 1080">
+    <div className="masked-img-container">
+      <svg width={width} height={height} x="0px" y="0px" viewBox="0 0 1920 1080" >
         <g>
           <defs>
             <polygon id="mask" points="974.66,0 960,20 945.34,0 0,0 0,1080 1920,1080 1920,0"/>
@@ -11,17 +12,29 @@ function MaskedImage({path, width, height}) {
           <clipPath id="clipPath">
             <use xlinkHref="#mask" />
           </clipPath>
-            <image width={width} height={height} clipPath="url(#clipPath)"  xlinkHref={path} ></image>
+          	<a xlinkHref={link} target="_blank">
+            	<image width="1920" height="1080" clipPath="url(#clipPath)" xlinkHref={path} ></image>
+          	</a>
         </g>
       </svg>
+    </div>
    )
 }
 
 function App() {
   return (
      <div>
-        <MaskedImage width="1920px" height="1080px" path="http://media.marche-int.com/image/upload/q_60/karma_test/he29f9bxydz33tdm3bx5.jpg" />
-        <MaskedImage width="1920px" height="1080px" path="http://media.marche-int.com/image/upload/q_60/karma_test/jn2lifsk9clqqcp4f1w1.png" />
+        <MaskedImage 
+        	width="1920px" 
+        	height="1080px" 
+        	path="http://media.marche-int.com/image/upload/q_60/karma_test/he29f9bxydz33tdm3bx5.jpg"
+        	link="https://www.facebook.com/WhiteMonkeyPizza/" 
+      	/>
+        <MaskedImage 
+        	width="1920px" 
+        	height="1080px" 
+        	path="http://media.marche-int.com/image/upload/q_60/karma_test/jn2lifsk9clqqcp4f1w1.png" 
+      	/>
      </div>
     )
 }
